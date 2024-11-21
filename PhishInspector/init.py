32c7598738740ca0c2bc6724ce9b083279c1600a
Muhammad Sameer <awaisahmed@Awaiss-MacBook-Pro.local>
2024-11-21 10:00:57 +0500

    Update the init.py file

--- PhishInspector/init.py
+++ PhishInspector/init.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+#Database credentials and connection
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sam:1122@localhost/pishing'
 app.config['SECRET_KEY'] = 'your_secret_key'
