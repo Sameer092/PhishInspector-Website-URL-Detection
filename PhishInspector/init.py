d9b9496af3e95703663bacf7b0a8a8f92b740635
Muhammad Sameer <awaisahmed@Awaiss-MacBook-Pro.local>
2024-12-03 14:03:00 +0500

    Update __init__.py to use environment variables

--- PhishInspector/init.py
+++ PhishInspector/init.py
@@ -1,11 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-#vAdd the Database postgres credentials and connection
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sam:1122@localhost/pishing'
-app.config['SECRET_KEY'] = 'your_secret_key'
+
+# Use environment variables
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://sam:1122@localhost/pishing')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
 
 db = SQLAlchemy(app)
